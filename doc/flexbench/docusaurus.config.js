588d8061ab179e470cfc810fbd94db3124229de6
Yujun Liu <yujunliu150@gmail.com>
2024-09-01 16:19:06 -0400

    fix the docusaurus script

--- doc/flexbench/docusaurus.config.js
+++ doc/flexbench/docusaurus.config.js
@@ -1,21 +1,21 @@
-// @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
-
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+const organizationName = process.env.GITHUB_ORG_NAME || 'flexivian'; // Default to 'flexivian' if not set
+const projectName = process.env.GITHUB_PROJECT_NAME || 'flexbench'; // Default to 'flexbench' if not set
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Flexbench',
   tagline: 'Simulation is awesome 🔥',
-  url: 'https://flexivial.github.io/',
-  baseUrl: '/flexbench/',
+  url: `https://${organizationName}.github.io/`,
+  baseUrl: `/${projectName}/`,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
-  organizationName: 'flexivian',
-  projectName: 'flexbench',
+  organizationName,
+  projectName,
 
   i18n: {
     defaultLocale: 'en',
@@ -25,85 +25,80 @@ const config = {
   presets: [
     [
       'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/flexivian/flexbench',
+          editUrl: `https://github.com/${organizationName}/${projectName}`,
         },
         blog: {
           showReadingTime: true,
-          editUrl:
-            'https://github.com/flexivian/flexbench',
+          editUrl: `https://github.com/${organizationName}/${projectName}`,
         },
-      }),
+      },
     ],
   ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'Flexbench',
-        logo: {
-          alt: 'Flexbench Logo',
-          src: 'img/logo.svg',
+  themeConfig: {
+    navbar: {
+      title: 'Flexbench',
+      logo: {
+        alt: 'Flexbench Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'intro',
+          position: 'left',
+          label: 'Docs',
         },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Docs',
-          },
-          { to: '/blog', label: 'Blog', position: 'left' },
-          {
-            href: 'https://github.com/flexivian/flexbench',
-            label: 'GitHub',
-            position: 'right',
-          },
-          {
-            type: 'localeDropdown',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Introduction',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {},
-          {},
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/flexivian/flexbench',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Flexbench`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+        { to: '/blog', label: 'Blog', position: 'left' },
+        {
+          href: `https://github.com/${organizationName}/${projectName}`,
+          label: 'GitHub',
+          position: 'right',
+        },
+        {
+          type: 'localeDropdown',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Introduction',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {},
+        {},
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: `https://github.com/${organizationName}/${projectName}`,
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Flexbench`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
 };
 
 module.exports = config;
